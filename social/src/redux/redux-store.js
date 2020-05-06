4c8b59c9f27813888e82637accc6cbe19bb929f0
DanilovAlexey <danilov_av90@list.ru>
2020-05-06 11:31:33 +0300

    75. redux-form для логина. начало

--- social/src/redux/redux-store.js
+++ social/src/redux/redux-store.js
@@ -5,13 +5,15 @@ import sideBarReducer from './sideBarReducer'
 import usersReducer from './usersReducer'
 import authReducer from './authReducer'
 import thunkMiddleWare from 'redux-thunk'
+import { reducer as formReducer } from 'redux-form'
 
 const reducers = combineReducers({
     profilePage: profileReducer, 
     dialogsPage: dialogReducer,
     sideBar: sideBarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 const store = createStore(reducers, applyMiddleware(thunkMiddleWare))
