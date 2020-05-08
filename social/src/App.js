9dca7986f05f34dd4cbe6dba4f81478c56efc5e3
DanilovAlexey <danilov_av90@list.ru>
2020-05-08 13:31:30 +0300

    80. инициализация приложения

--- social/src/App.js
+++ social/src/App.js
@@ -7,34 +7,52 @@ import Footer from './components/Footer/Footer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, withRouter } from 'react-router-dom'
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import Login from './components/Login/Login';
+import { initializeApp } from './redux/appReducer'
+import { connect } from 'react-redux'
+import { compose } from 'redux';
+import Preloader from './components/common/Preloader/Preloader';
 
 
-function App(props) {
-  return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <HeaderContainer />
-        {<NavbarContainer />}
-        <div className="app-wrapper-content">
-          {/* <Route path="/profile" component={Profile} /> */}
-          {/* <Route path="/dialogs" component={Dialogs} /> */}
-          <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
-          <Route path="/dialogs" render={() => <DialogsContainer />} />
-          <Route path="/users" render={() => <UsersContainer />} />
-          <Route path="/news" component={News} />
-          <Route path="/music" component={Music} />
-          <Route path="/settings" component={Settings} />
-          <Route path="/login" component={() => <Login />} />
-          <Route path="/" exact render={() => <ProfileContainer />} />
+class App extends React.Component {
+  componentDidMount() {
+    this.props.initializeApp()
+  }
+
+  render() {
+    
+    if (!this.props.initialized)
+    {return <Preloader />}
+
+    return (  
+        <div className="app-wrapper">
+          <HeaderContainer />
+          {<NavbarContainer />}
+          <div className="app-wrapper-content">
+            {/* <Route path="/profile" component={Profile} /> */}
+            {/* <Route path="/dialogs" component={Dialogs} /> */}
+            <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
+            <Route path="/dialogs" render={() => <DialogsContainer />} />
+            <Route path="/users" render={() => <UsersContainer />} />
+            <Route path="/news" component={News} />
+            <Route path="/music" component={Music} />
+            <Route path="/settings" component={Settings} />
+            <Route path="/login" component={() => <Login />} />
+            <Route path="/" exact render={() => <ProfileContainer />} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  initialized: state.app.initialized
+})
+
+export default compose(
+    withRouter,
+    connect(mapStateToProps, { initializeApp }))(App);
